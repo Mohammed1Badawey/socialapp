7784db0d12e1966eb8321d74fb810eaa17f907c7
Mohammed1Badawey <MuhammadMohamed32@hotmail.com>
2025-03-18 10:14:44 +0200

    feat(PostCard): add delete post functionality
    Implement the ability to delete a post from the PostCard component. This includes adding the deletePostSlice to the Redux store, creating the deletePostSlice with async thunk for handling the deletion, and updating the PostCard component to dispatch the delete action. The user's posts are refreshed after deletion to ensure the UI reflects the changes.

---
+++ src/lib/Redux/Slices/deletePostSlice.ts
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { authAxios } from "@/AxiosConfig/AxiosConfig";
+const initialState = {
+  id: "",
+  isLoading: false,
+};
+export const deletePost = createAsyncThunk(
+  "deletePostSlice/deletePost",
+  async (id: any, { rejectWithValue }) => {
+    try {
+      const res = await authAxios.delete(`/posts/${id}`);
+      console.log("res", res);
+      return res.data.post.id;
+    } catch (err: any) {
+      console.log("err", err);
+      return rejectWithValue(err.response?.data?.error || "An error occurred");
+    }
+  }
+);
+
+const deletePostSlice = createSlice({
+  name: "post",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(deletePost.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(deletePost.fulfilled, (state,action) => {
+      state.isLoading = false;
+      state.id = action.payload;
+      console.log("state", action.payload);
+      
+    });
+    builder.addCase(deletePost.rejected, (state) => {
+      state.isLoading = false;
+    });
+  },
+});
+
+export default deletePostSlice.reducer;
