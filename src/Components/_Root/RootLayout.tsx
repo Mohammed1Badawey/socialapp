ef4179550941b15cb9b1a5c5d133012472301301
Mohammed1Badawey <MuhammadMohamed32@hotmail.com>
2025-03-27 05:22:54 +0200

    refactor(ThemeToggle): remove ThemeToggle from RootLayout and enhance UI
    The ThemeToggle component has been removed from the RootLayout to simplify the layout structure. Additionally, the ThemeToggle UI has been enhanced by adding text labels for "Dark Mode" and "Light Mode" alongside the icons for better user clarity.Also, add ThemeToggle component to Navbar and update

--- src/Components/_Root/RootLayout.tsx
+++ src/Components/_Root/RootLayout.tsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-import ThemeToggle from "../ThemeToggle";
 import Navbar from './../Navbar/Navbar';
 
 const RootLayout = () => {
@@ -7,9 +6,7 @@ const RootLayout = () => {
     <div className="relative min-h-screen w-full">
       <Navbar />
       {/* Theme toggle positioned in the top-right corner */}
-      <div className="absolute top-4 right-4 z-10">
-        <ThemeToggle />
-      </div>
+
       <section className="flex min-h-full">
       <Outlet />
       </section>
